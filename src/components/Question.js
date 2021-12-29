25a62e1a1e24600e6e6db66b71b607eb0c2fdfb9
Andy Chan <andy.chan9930@gmail.com>
2021-12-29 15:55:22 -0500

    commit tree de11b8f7c4a178448b266a7e99370d70dc0f1c9c

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(()=> {
+    if(timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+      return
+    }
+
+    const timerID = setTimeout(()=> {
+      setTimeRemaining((timeRemaining)=>timeRemaining - 1)
+    },1000)
+    return () => clearTimeout(timerID)
+  },[timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
